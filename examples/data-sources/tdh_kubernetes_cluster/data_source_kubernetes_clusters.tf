0502f0358ae419a09b525f1fe9eaf7cfd9bf4179
shreyas3-vmw <132661158+shreyas3-vmw@users.noreply.github.com>
2024-06-14 16:26:16 +0530

    Datasource: Kubernetes Cluster List

--- examples/data-sources/tdh_kubernetes_cluster/data_source_kubernetes_clusters.tf
+++ examples/data-sources/tdh_kubernetes_cluster/data_source_kubernetes_clusters.tf
@@ -1,21 +1,5 @@
 
-terraform {
-  required_providers {
-    tdh = {
-      source = "hashicorp.com/svc-bot-mds/tdh"
-    }
-  }
-}
 
-provider "tdh" {
-  host      = "https://tdh-cp-dev.tdh.nn.com/"
-
-  username = "sre@broadcom.com"
-  password = "Admin!23"
-  org_id = "32fb14b6-13c8-4821-a4b3-e7b693327061"
-
-  type = "user_creds"
-}
 
 data "tdh_kubernetes_clusters" "all"{
   account_id = "58570f12-14b8-4f39-b226-dcd0ac4c4560"
